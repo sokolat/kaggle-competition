ace6ccdd0340867cb76b4a018fc2bf12905ce769
rango <sidya.galakho@umontreal.ca>
2025-02-08 01:44:12 -0500

    implement fake image detection model with transfer learning

--- wai-challenge/fake_image_detection.py
+++ wai-challenge/fake_image_detection.py
@@ -2,47 +2,102 @@ import os
 from typing import Optional
 
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
-from torchvision.models import DenseNet121_Weights, densenet121
+from tqdm import tqdm
+
+from xception import xception
 
 
 class ImageDataset(Dataset):
-    def __init__(self, folder: str, transform=None):
+    def __init__(self, folder: str, csv_file: str, prefix: str, transform=None):
         self.folder = folder
-        self.image_files = [filename for filename in os.listdir(folder)]
+        self.csv_file = csv_file
+        self.prefix = prefix
+        self.filenames = [
+            filename for filename in os.listdir(os.path.join(self.folder, self.prefix))
+        ]
+        self.labels = pd.read_csv(
+            os.path.join(self.folder, self.csv_file), delimiter=","
+        )
+        self.filenames_to_labels_map = {}
+
+        for _, row in self.labels.iterrows():
+            self.filenames_to_labels_map[row[1]] = row[2]
         self.transform = transform
 
     def __len__(self):
-        return len(self.image_files)
+        return len(self.filenames)
 
-    def __getitem__(self, index):
-        img_path = os.path.join(self.folder, self.image_files[index])
+    def __getitem__(self, index: int):
+        img_path = os.path.join(self.folder, self.prefix, self.filenames[index])
         image = Image.open(img_path).convert("RGB")
         if self.transform:
             image = self.transform(image)
-        return image
+        return (
+            image,
+            self.filenames_to_labels_map[self.prefix + "/" + self.filenames[index]],
+        )
 
 
-class Model(nn.Module):
+class Network(nn.Module):
     def __init__(self):
-        super(Model, self).__init__()
-        self.base_model = densenet121(weights=DenseNet121_Weights.IMAGENET1K_V1)
+        super(Network, self).__init__()
+        self.base_network = xception()
         self.num_classes = 2
-        for param in self.base_model.parameters():
+        for param in self.base_network.parameters():
             param.requires_grad = False
-        in_features = self.base_model.classifier.in_features
-        self.base_model.classifier = nn.Linear(in_features, self.num_classes)
+        in_features = self.base_network.last_linear.in_features
+        self.base_network.last_linear = nn.Linear(in_features, self.num_classes)
+        conv4_params = self.base_network.conv4.parameters()
+        last_linear_params = self.base_network.last_linear.parameters()
+
+        for param in last_linear_params:
+            param.requires_grad = True
+        for param in conv4_params:
+            param.requires_grad = True
 
 
 if __name__ == "__main__":
     transform = transforms.Compose(
         [transforms.PILToTensor(), transforms.Resize((224, 224))]
     )
-    dataset = ImageDataset(folder="./train_data", transform=transform)
-    dataloader = DataLoader(dataset, batch_size=128, shuffle=True, pin_memory=True)
-    model = Model()
-    print(model.base_model)
+
+    train_dataset = ImageDataset(
+        folder="data",
+        csv_file="train.csv",
+        prefix="train_data",
+        transform=transform,
+    )
+    # test_dataset = ImageDataset(
+    #    folder="data", csv_file="test.csv", prefix="test_data_v2", transform=transform
+    # )
+
+    dataloader = DataLoader(
+        train_dataset, batch_size=128, shuffle=True, pin_memory=True
+    )
+
+    """
+    state_dict = torch.load("./data/xception-b5690688.pth")
+
+    for name, weights in state_dict.items():
+        if "pointwise" in name:
+            state_dict[name] = weights.unsqueeze(-1).unsqueeze(-1)
+    torch.save(state_dict, "xception-fixed.pth")
+    """
+
+    network = Network()
+    learning_rate = 0.001
+    optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
+    train_logs = {
+        "train_loss": [],
+        "test_loss": [],
+        "train_f1_score": [],
+        "test_f1_score": [],
+    }
+
+    breakpoint()
