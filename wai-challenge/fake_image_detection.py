80c55442ea1446b59c1a14757dd6af555d10c400
rango <sidya.galakho@umontreal.ca>
2025-02-11 18:35:13 -0500

    unfreeze conv4 weights

--- wai-challenge/fake_image_detection.py
+++ wai-challenge/fake_image_detection.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 from typing import Optional
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
@@ -12,6 +13,7 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.sampler import SubsetRandomSampler
+from torcheval.metrics.functional import multiclass_f1_score
 from torchvision import transforms
 from tqdm import tqdm
 
@@ -60,15 +62,11 @@ def parse_args():
 
 
 class ImageDataset(Dataset):
-    def __init__(self, prefix: str, transform=None):
-        self.prefix = prefix
+    def __init__(self, transform=None):
         self.filenames = [
-            filename
-            for filename in os.listdir(os.path.join("data", self.prefix + "_data"))
+            filename for filename in os.listdir(os.path.join("data/train_data"))
         ]
-        self.labels = pd.read_csv(
-            os.path.join("data", self.prefix + ".csv"), delimiter=","
-        )
+        self.labels = pd.read_csv(os.path.join("data/train.csv"), delimiter=",")
         self.filenames_to_labels_map = {}
 
         for _, row in self.labels.iterrows():
@@ -79,15 +77,13 @@ class ImageDataset(Dataset):
         return len(self.filenames)
 
     def __getitem__(self, index: int):
-        img_path = os.path.join("data", self.prefix + "_data", self.filenames[index])
+        img_path = os.path.join("data/train_data", self.filenames[index])
         image = Image.open(img_path).convert("RGB")
         if self.transform:
             image = self.transform(image)
         return (
             image,
-            self.filenames_to_labels_map[
-                self.prefix + "_data" + "/" + self.filenames[index]
-            ],
+            self.filenames_to_labels_map["train_data" + "/" + self.filenames[index]],
         )
 
 
@@ -99,7 +95,9 @@ class Network(nn.Module):
         for param in self.base_network.parameters():
             param.requires_grad = False
         in_features = self.base_network.last_linear.in_features
-        self.base_network.last_linear = nn.Linear(in_features, self.num_classes)
+        self.base_network.last_linear = nn.Sequential(
+            nn.Dropout(p=0.5), nn.Linear(in_features, self.num_classes)
+        )
         conv4_params = self.base_network.conv4.parameters()
         last_linear_params = self.base_network.last_linear.parameters()
 
@@ -130,18 +128,22 @@ if __name__ == "__main__":
         else:
             device = torch.device("mps")
 
-    print(f"INITIALIZING TRAINING ON MPS GPU")
+    elif args.device == "cuda":
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print(f"INITIALIZING TRAINING ON {args.device.upper()} GPU")
     start_time = datetime.now()
     print(f"Start Time: {start_time}")
 
     transform = transforms.Compose(
-        [transforms.PILToTensor(), transforms.Resize((299, 299))]
+        [
+            transforms.ToTensor(),
+            transforms.Resize((299, 299)),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
     )
 
-    dataset = ImageDataset(
-        prefix="train",
-        transform=transform,
-    )
+    dataset = ImageDataset(transform=transform)
 
     validation_split = 0.2
     dataset_size = len(dataset)
@@ -154,37 +156,35 @@ if __name__ == "__main__":
     train_sampler = SubsetRandomSampler(train_indices)
     valid_sampler = SubsetRandomSampler(val_indices)
     train_dataloader = DataLoader(
-        dataset, batch_size=args.batch_size, sampler=train_sampler, pin_memory=True
+        dataset, batch_size=args.batch_size, sampler=train_sampler
     )
     val_dataloader = DataLoader(
-        dataset, batch_size=args.batch_size, sampler=valid_sampler, pin_memory=True
+        dataset, batch_size=args.batch_size, sampler=valid_sampler
     )
 
     model = Network().base_network.to(device)
     optimizer = torch.optim.Adam(
-        model.parameters(), lr=args.learning_rate, weight_decay=0.0001
+        model.parameters(), lr=args.learning_rate, weight_decay=0.00001
     )
     criterion = nn.CrossEntropyLoss()
-    epoch_loss = 0.0
-    epoch_f1_score = 0.0
-
-    valid_loss = 0.0
-    valid_f1_score = 0.0
 
     valid_loss_min = np.inf
     train_losses = []
     valid_losses = []
     train_f1_scores = []
-    val_f1_scores = []
-    for epoch in range(1, args.epochs + 1):
+    valid_f1_scores = []
+    for epoch in tqdm(range(1, args.epochs + 1)):
         print(f"{'='*50}")
         print(f"EPOCH {epoch} - TRAINING...")
 
+        epoch_loss = 0.0
+        epoch_f1_score = 0.0
+
         model.train()
-        for index, (data, target) in enumerate(train_dataloader):
+        for data, target in tqdm(train_dataloader):
             data = data.to(device, dtype=torch.float32)
             target = target.to(device, dtype=torch.long)
-
+            breakpoint()
             optimizer.zero_grad()
 
             output = model(data)
@@ -192,9 +192,10 @@ if __name__ == "__main__":
 
             loss.backward()
 
-            f_score = f1_score(target.cpu(), output.cpu().argmax(dim=1))
+            # f_score = f1_score(target.cpu(), output.cpu().argmax(dim=1))
+            f_score = multiclass_f1_score(output, target, num_classes=2)
             epoch_loss += loss.item()
-            epoch_f1_score += f_score
+            epoch_f1_score += f_score.item()
 
             optimizer.step()
 
@@ -209,18 +210,22 @@ if __name__ == "__main__":
 
         print(f"EPOCH {epoch} - VALIDATING...")
 
+        valid_loss = 0.0
+        valid_f1_score = 0.0
+
         model.eval()
 
-        for data, target in val_dataloader:
-            data = data.to(self.device, dtype=torch.float32)
-            target = target.to(self.device, dtype=torch.long)
+        for data, target in tqdm(val_dataloader):
+            data = data.to(device, dtype=torch.float32)
+            target = target.to(device, dtype=torch.long)
 
             with torch.no_grad():
                 output = model(data)
                 loss = criterion(output, target)
-                f_score = f1_score(target.cpu(), output.cpu().argmax(dim=1))
+                # f_score = f1_score(target.cpu(), output.cpu().argmax(dim=1))
+                f_score = multiclass_f1_score(output, target, num_classes=2)
                 valid_loss += loss.item()
-                valid_f1_score += f_score
+                valid_f1_score += f_score.item()
 
         val_loss, val_f1_socre = valid_loss / len(val_dataloader), valid_f1_score / len(
             val_dataloader
